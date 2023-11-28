338c696e2a380b1096a355fc223fd96428a44839
Bruno <brunoandreotti@outlook.com>
2023-11-28 18:34:46 -0300

    feat: criado repository de game e gametrack

---
+++ src/main/java/com/brunoandreotti/gametracker/domain/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.brunoandreotti.gametracker.domain.repositories;
+
+import com.brunoandreotti.gametracker.domain.models.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
